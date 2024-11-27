46a09ef74c7fabea7d56732c41e2d8de6a50d845
Rahul Mahato <mahatorahul025@gmail.com>
2024-11-27 15:31:44 +0530

    fixed typescript issue

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const ONRENDER_URL = import.meta.env.VITE_ONRENDER_SERVER_DEV_URL;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const AWS_URL = import.meta.env.VITE_ONRENDER_SERVER_DEV_URL;
+// const AWS_URL = import.meta.env.VITE_ONRENDER_SERVER_DEV_URL;
 const LOCAL_URL = import.meta.env.VITE_LOCAL_SERVER_DEV_URL;
 
 console.log(LOCAL_URL);
