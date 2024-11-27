8034ad6938738bca07604fb7f2e46c77fbe616c7
Rahul Mahato <mahatorahul025@gmail.com>
2024-11-27 16:12:30 +0530

    removed unwnted log

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const ONRENDER_URL = import.meta.env.VITE_ONRENDER_SERVER_DEV_URL;
 // const AWS_URL = import.meta.env.VITE_ONRENDER_SERVER_DEV_URL;
-const LOCAL_URL = import.meta.env.VITE_LOCAL_SERVER_DEV_URL;
-
-console.log(LOCAL_URL);
+// const LOCAL_URL = import.meta.env.VITE_LOCAL_SERVER_DEV_URL;
 
 export const instance = axios.create({
   baseURL: ONRENDER_URL,
